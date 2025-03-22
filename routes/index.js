31cea786c107589768fefdb59910ab715ca8633b
Vadim <58843818+vadim-shev@users.noreply.github.com>
2025-03-22 20:39:49 +0200

    1

--- routes/index.js
+++ routes/index.js
@@ -25,9 +25,9 @@ export default {
     								<video preload="none" id="video" style="height: calc(100% + 55px); margin-top: -55px; width:100%; z-index: 10; object-fit: cover; opacity: .6;" autoplay="autoplay" muted="muted" loop="loop" playsinline="" loading="lazy" ><source src="assets/models/video.mp4" type="video/mp4"></video>
 		            	</div>    
 		            	<div style="background: rgba(0,0,0,0.95); width: 100vw; height: 75vh; z-index: 100; display: flex; flex-direction: column; justify-content: center; align-content: center; align-items: flex-end;">
-		            		<h1 style="font-size: 3rem;  width: 100vw; height: 25vh; text-align: left; text-transform: uppercase;">granitblizkim</h1>
-		            		<h2  style="font-size: 2rem; width: auto; height: 25vh; margin: 0 5px 0 5px; text-align: left;">Обслуживание с достоинством и уважением</h2>
-                                    <button class="prime_btn"  style="height: auto; width: auto;  font-size: 2.5rem; color: black; background: rgba(255, 255, 255, .5); padding: 10px 20px; z-index: 200; border-radius: 0px;  " @click="scrollAction('catalog')" >Перейти к каталогу</button>
+		            		<h1 class="prime_highline">granitblizkim</h1>
+		            		<h2 class="secondary_highline" style="">Обслуживание с достоинством и уважением</h2>
+                                    <button class="prime_btn"  style="  " @click="scrollAction('catalog')" >Перейти к каталогу</button>
 		            	</div> 
 		            </div>
 		        </div> 
